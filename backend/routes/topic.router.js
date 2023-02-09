5d9be24295c7ae25f3f9de5c15b9807c24ea0cb1
Vasilev Artyom <barkqaqa@gmail.com>
2023-02-09 16:50:47 +0300

    create Router for Topics

---
+++ backend/routes/topic.router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Topic, Question } = require('../db/models');
+
+router.get('/', async (req, res) => {
+  try {
+    const Topics = await Topic.findAll({
+      include:
+             {
+               model: Question,
+               key: 'topic_id',
+             },
+    });
+    res.status(200).json(Topics);
+  } catch ({ message }) {
+    res.status(500).json(message);
+  }
+});
